a1950317f7a6b26717e7896667fc52dd49389141
Meow_nyang <slah76@naver.com>
2025-04-24 09:49:48 +0900

    Header, Footer, Home 생성, Router 활성화(컴포넌트 형식으로)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <Header />
+      <div className='content-wrapper'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
