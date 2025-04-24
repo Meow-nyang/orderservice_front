ff355e33b6699b40e4c03e77be08c5e7b906008a
Meow_nyang <slah76@naver.com>
2025-04-24 12:46:05 +0900

    Header 메뉴 추가, 조건부 렌더링, 로그아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './components/Home';
+import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
 import MemberCreate from './components/MemberCreate';
+import { AuthContextProvider } from './context/UserContext';
+import LoginPage from './components/LoginPage';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Header />
-      <div className='content-wrapper'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/member/create' element={<MemberCreate />} />
-        </Routes>
+    <AuthContextProvider>
+      <div className='App'>
+        <Header />
+        <div className='content-wrapper'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/member/create' element={<MemberCreate />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </AuthContextProvider>
   );
 };
 
